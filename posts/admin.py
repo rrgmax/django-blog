4d2af48f3d47ab126458ffa1c2ebfd4550260733
Rogerio <rrgmax@bol.com.br>
2019-02-15 19:37:08 -0200

    Define list_display variable in the PostModelAdmin class

--- posts/admin.py
+++ posts/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "timestamp")
     class Meta:
         model = Post
 
