b304e755bda1faee2e883ae295559558b8928083
Rogerio <rrgmax@bol.com.br>
2019-02-15 19:10:02 -0200

    Create Post model into models.py file

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+    #def __unicode__(self):
+        #return self.title
+
+    def __str__(self):
+        return self.title
