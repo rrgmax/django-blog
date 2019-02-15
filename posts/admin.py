bc937067770054bc9ddd5d337101cc305ba3d3da
Rogerio <rrgmax@bol.com.br>
2019-02-15 19:39:21 -0200

    Update list_display into admin.py file

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "timestamp")
+    list_display = ("title", "updated", "timestamp")
     class Meta:
         model = Post
 
