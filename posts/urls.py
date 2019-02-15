9f88def26b01bb9d29064418e6709590d96201ba
Rogerio <rrgmax@bol.com.br>
2019-02-15 21:40:56 -0200

    Dafine some views into posts\views.py and set it into posts\urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.post_home),
+    path('list/', views.post_list),
+    path('create/', views.post_create),
+    path('detail/', views.post_detail),
+    path('update/', views.post_update),
+    path('delete/', views.post_delete)
 ]
    
