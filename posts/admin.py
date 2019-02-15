eb97c13a7cca92bc392bc199a9b71822a30600d7
Rogerio <rrgmax@bol.com.br>
2019-02-15 20:19:23 -0200

    Insert some functions in the PostModelAdmin into admin.py file

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,11 @@ from django.contrib import admin
 from .models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ("title", "updated", "timestamp")
+    list_display = ["title", "updated", "timestamp"]
+    list_display_links = ["updated"]
+    list_editable = ["title"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
     class Meta:
         model = Post
 
