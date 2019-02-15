7147655c7d2f2d2dc0c9afd324ac7dd495a8d363
Rogerio <rrgmax@bol.com.br>
2019-02-15 19:13:48 -0200

    Define max_length attribute in the class Posts title variable

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
