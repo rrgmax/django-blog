cd23b908e016d176cb2c683794f2a8ebcfb1aab7
Rogerio <rrgmax@bol.com.br>
2019-02-16 18:23:47 -0200

    Update views.py and post_form.html

--- posts/views.py
+++ posts/views.py
@@ -5,15 +5,16 @@ from .forms import PostForm
 from .models import Post
 # Create your views here.
 def post_create(request):
-    form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()   
     context = {
         "form": form,
-
     }
     return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-    #instance = Post.objects.get(id=1)
     instance = get_object_or_404(Post, id=id)
     context = {
         "title": instance.title,
@@ -27,18 +28,21 @@ def post_list(request):
         "object_list": queryset,
         "title": "List"
     }
-    # if request.user.is_authenticated:
-    #     context = {
-    #         "title": "My User List"
-    #     }
-    # else:
-    #     context = {
-    #         "title": "List"
-    #     }
     return render(request, "index.html", context)
     
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")
