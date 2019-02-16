9b227a3511ecbe893cab8554428d2e7aa81ce0e2
Rogerio <rrgmax@bol.com.br>
2019-02-16 01:27:38 -0200

    Insert context instance in he post_detail into views.py

--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,9 @@ from .models import Post
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
+def post_detail(request, id=None):
     #instance = Post.objects.get(id=1)
-    instance = get_object_or_404(Post, id=3)
+    instance = get_object_or_404(Post, id=id)
     context = {
         "title": instance.title,
         "instance": instance,
